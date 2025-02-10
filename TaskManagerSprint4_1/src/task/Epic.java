4103cf442cbd8abc6502cf35cdaae6519b8f6bc7
DeanAshton28 <115179676+DeanAshton28@users.noreply.github.com>
2025-02-10 15:39:35 +0300

    Add files via upload

---
+++ TaskManagerSprint4_1/src/task/Epic.java
@@ -0,0 +1,72 @@
+package task;
+
+
+
+import java.util.ArrayList;
+
+
+
+public class Epic extends Task {
+
+    private ArrayList<Subtask> subtaskList = new ArrayList<>();
+
+
+
+    public Epic(String name, String description) {
+
+        super(name, description);
+    }
+
+
+
+    public void addSubtask(Subtask subtask) {
+
+        subtaskList.add(subtask);
+
+    }
+
+
+
+    public void clearSubtasks() {
+
+        subtaskList.clear();
+
+    }
+
+
+
+    public ArrayList<Subtask> getSubtaskList() {
+
+        return new ArrayList<>(subtaskList);
+
+    }
+
+
+
+    @Override
+
+    public String toString() {
+
+        return "Epic{" +
+
+                "name='" + getName() + '\'' +
+
+                ", description='" + getDescription() + '\'' +
+
+                ", id=" + getId() +
+
+                ", subtaskList.size=" + subtaskList.size() +
+
+                ", status=" + getStatus() +
+
+                '}';
+
+    }
+
+
+
+    public void setStatus(Status status) {
+
+    }
+
+}
